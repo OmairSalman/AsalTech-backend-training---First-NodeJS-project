7c45641f3d325e23b44aade16886e3f288ccafe9
Omair Salman <az93571@gmail.com>
2025-08-27 10:25:55 +0300

    removed unused DATABASE_PORT env variable

--- src/config/environment.d.ts
+++ src/config/environment.d.ts
@@ -3,7 +3,6 @@ declare namespace NodeJS {
     NODE_ENV: string;
     PORT: number;
     DATABASE_HOST: string;
-    DATABASE_PORT: number;
     DATABASE_USERNAME: string;
     DATABASE_PASSWORD: string;
     DATABASE_NAME: string;
