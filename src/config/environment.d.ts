885af96acbb8b416ec88c4aa1332918974f56cf7
Omair <az93571@gmail.com>
2025-09-13 11:00:08 +0300

    removed mongo database url environment variable declaration. updated version of axios in package-log via npm audit fix.

--- src/config/environment.d.ts
+++ src/config/environment.d.ts
@@ -4,7 +4,6 @@ declare namespace NodeJS {
     DATABASE_USERNAME: string;
     DATABASE_PASSWORD: string;
     DATABASE_NAME: string;
-    DATABASE_URL: string;
     ACCESS_TOKEN_SECRET: string;
     REFRESH_TOKEN_SECRET: string;
     GRAVATAR_API_KEY: string;
